10bfa10d294203cc365d58e7d624aedbbf1991b9
Andreas Jonson <andjo403@users.noreply.github.com>
2019-11-07 22:33:48 +0100

    another collaps algorithm

--- tools_lib/src/stack_collapse.rs
+++ tools_lib/src/stack_collapse.rs
@@ -1,117 +1,79 @@
-use std::collections::{HashMap, HashSet};
-use std::time::{Duration, SystemTime};
-use std::convert::TryInto;
+use std::cmp;
+use std::collections::HashMap;
+use std::time::Duration;
+use std::time::SystemTime;
 
 use measureme::{Event, ProfilingData};
-
-
-fn find_all_thread_ids_and_start_times(profiling_data: &ProfilingData) -> HashMap<u64, SystemTime> {
-
-    let mut thread_ids = HashMap::new();
-
-    for event in profiling_data.iter().filter(|e| !e.timestamp.is_instant()) {
-        thread_ids
-            .entry(event.thread_id)
-            .and_modify(|start| {
-                *start = std::cmp::min(event.timestamp.start(), *start);
-            })
-            .or_insert(event.timestamp.start());
-    }
-
-    thread_ids
-}
-
-fn process_thread(
-    thread_id: u64,
-    thread_start_time: SystemTime,
-    profiling_data: &ProfilingData,
-    sampling_interval: Duration,
-    counters: &mut HashMap<String, u64>,
-) {
-
-    let mut events = profiling_data
-        .iter()
-        .rev()
-        .filter(|e| !e.timestamp.is_instant())
-        .filter(|e| e.thread_id == thread_id)
-        .peekable();
-
-    let mut sample_cursor = if let Some(last_event) = events.peek() {
-        // Start sample (backwards) at the last position that we would hit if
-        // we sampled forward starting from `thread_start_time`.
-
-        let max_sampled_timestamp = last_event.timestamp.end() - Duration::from_nanos(1);
-
-        if max_sampled_timestamp < thread_start_time {
-            // Just ignore this zero-length thread
-            return
-        }
-
-        let sampling_interval_nanos = sampling_interval.as_nanos();
-
-        let thread_duration_nanos = max_sampled_timestamp.duration_since(thread_start_time).unwrap().as_nanos();
-        let samples = thread_duration_nanos / sampling_interval_nanos;
-        thread_start_time + Duration::from_nanos((samples * sampling_interval_nanos).try_into().unwrap())
-    } else {
-        // No events? Ignore the thread.
-        return
-    };
-
-    let mut stack: Vec<Event<'_>> = vec![];
-    // The id is updated in sync we `stack`
-    let mut stack_id = "rustc".to_owned();
-
-    loop {
-
-        // Pop things from the stack that we've moved past
-        while let Some(top) = stack.last().cloned() {
-            if top.timestamp.contains(sample_cursor) {
-                break
-            } else {
-                let popped = stack.pop().unwrap();
-                let new_stack_id_len = stack_id.len() - (popped.label.len() + 1);
-                stack_id.truncate(new_stack_id_len);
-            }
-        }
-
-        // Push things onto the stack that are under the cursor now
-        while let Some(event) = events.peek().cloned() {
-            if sample_cursor >= event.timestamp.end() {
-                // The next event is not under the cursor, so break
-                break
-            }
-
-            // Check if the event is under the cursor. It could also be an
-            // event that the cursor has skipped entirely
-            if event.timestamp.contains(sample_cursor) {
-                stack_id.push(';');
-                stack_id.push_str(&event.label[..]);
-                stack.push(event);
-            }
-
-            events.next();
-        }
-
-        if events.peek().is_none() && stack.is_empty() {
-            break
-        }
-
-        eprintln!("{}", stack_id);
-        *counters.entry(stack_id.clone()).or_insert(0) += 1;
-
-        sample_cursor -= sampling_interval;
-    }
-}
 
 pub fn collapse_stacks<'a>(
     profiling_data: &ProfilingData,
     sampling_interval: Duration,
 ) -> HashMap<String, u64> {
-    let thread_ids = find_all_thread_ids_and_start_times(profiling_data);
     let mut counters = HashMap::new();
+    let mut threads = HashMap::<_, (Vec<Event>, String, SystemTime, SystemTime, u64)>::new();
 
-    for (thread_id, thread_start_time) in thread_ids {
-        process_thread(thread_id, thread_start_time, profiling_data, sampling_interval, &mut counters);
+    for current_event in profiling_data
+        .iter()
+        .rev()
+        .filter(|e| !e.timestamp.is_instant())
+    {
+        let (thread_stack, stack_id, thread_start, _thread_end, total_event_time) =
+            threads.entry(current_event.thread_id).or_insert((
+                Vec::new(),
+                "rustc".to_owned(),
+                current_event.timestamp.start(),
+                current_event.timestamp.end(),
+                0,
+            ));
+
+        *thread_start = cmp::min(*thread_start, current_event.timestamp.start());
+
+        // Pop all events from the stack that are not parents of the
+        // current event.
+        while let Some(current_top) = thread_stack.last().cloned() {
+            if current_top.contains(&current_event) {
+                break;
+            }
+
+            let popped = thread_stack.pop().unwrap();
+            let new_stack_id_len = stack_id.len() - (popped.label.len() + 1);
+            stack_id.truncate(new_stack_id_len);
+        }
+
+        if !thread_stack.is_empty() {
+            // If there is something on the stack, subtract the current
+            // interval from it.
+            counters.entry(stack_id.clone()).and_modify(|self_time| {
+                eprintln!("sub {}", *self_time);
+                *self_time -= current_event.duration().unwrap().as_nanos() as u64;
+            });
+        }else{
+            // Update the total_event_time counter as the current event is on top level
+            *total_event_time += current_event.duration().unwrap().as_nanos() as u64;
+        }
+
+        // add this event to the stack_id
+        stack_id.push(';');
+        stack_id.push_str(&current_event.label[..]);
+
+        // update current events self time
+        let self_time = counters.entry(stack_id.clone()).or_default();
+        *self_time += current_event.duration().unwrap().as_nanos() as u64;
+
+        // Bring the stack up-to-date
+        thread_stack.push(current_event)
+    }
+
+    let mut rustc_time = 0;
+    for (_,(_, _, thread_start, thread_end, total_event_time)) in threads.iter() {
+        rustc_time +=
+            thread_end.duration_since(*thread_start).unwrap().as_nanos() as u64 - *total_event_time;
+    }
+    counters.insert("rustc".to_owned(), rustc_time);
+
+    let devisor = sampling_interval.as_nanos() as u64;
+    for (_, self_time) in counters.iter_mut() {
+        *self_time /= devisor;
     }
 
     counters
@@ -119,23 +81,63 @@ pub fn collapse_stacks<'a>(
 
 #[cfg(test)]
 mod test {
-    use measureme::{Event, ProfilingDataBuilder};
+    use measureme::ProfilingDataBuilder;
     use std::collections::HashMap;
-    use std::time::{Duration, SystemTime};
+    use std::time::Duration;
 
     #[test]
     fn basic_test() {
-
-
         let mut b = ProfilingDataBuilder::new();
 
-        //                       <--------->
-        //   <------------><-------------------->
-        //   100           300   400       600   700
+        //                                         <--e1-->
+        //                 <--e1-->        <----------e2---------->
+        //              T2 1       2       3       4       5       6
+        // sample interval |   |   |   |   |   |   |   |   |   |   |
+        // stacks count:
+        // rustc                       1   2
+        // rustc;e1            1   2
+        // rustc;e2                            1   2           3   4
+        // rustc;e2;e1                                 1   2
 
-        b.interval("Query", "EventA", 0, 100, 300, |_| {});
-        b.interval("Query", "EventB", 0, 300, 700, |b| {
-            b.interval("Query", "EventA", 0, 400, 600, |_| {});
+        b.interval("Query", "e1", 0, 100, 200, |_| {});
+        b.interval("Query", "e2", 0, 300, 600, |b| {
+            b.interval("Query", "e1", 0, 400, 500, |_| {});
+        });
+
+        let profiling_data = b.into_profiling_data();
+
+        let recorded_stacks = super::collapse_stacks(&profiling_data, Duration::from_nanos(50));
+
+        let mut expected_stacks = HashMap::<String, u64>::new();
+        expected_stacks.insert("rustc;e2;e1".into(), 2);
+        expected_stacks.insert("rustc;e2".into(), 4);
+        expected_stacks.insert("rustc;e1".into(), 2);
+        expected_stacks.insert("rustc".into(), 2);
+
+        assert_eq!(expected_stacks, recorded_stacks);
+    }
+
+    #[test]
+    fn multi_threaded_test() {
+        let mut b = ProfilingDataBuilder::new();
+
+        //                 <--e1-->        <--e1-->
+        //              T1 1       2       3       4       5
+        //                                 <--e3-->
+        //                 <--e1--><----------e2---------->
+        //              T2 1       2       3       4       5
+        // sample interval |       |       |       |       |
+        // stacks count:
+        // rustc                           1
+        // rustc;e1                2               3
+        // rustc;e2                        1               2
+        // rustc;e2;e3                             1
+
+        b.interval("Query", "e1", 1, 1, 2, |_| {});
+        b.interval("Query", "e1", 1, 3, 4, |_| {});
+        b.interval("Query", "e1", 2, 1, 2, |_| {});
+        b.interval("Query", "e2", 2, 2, 5, |b| {
+            b.interval("Query", "e3", 2, 3, 4, |_| {});
         });
 
         let profiling_data = b.into_profiling_data();
@@ -143,73 +145,11 @@ mod test {
         let recorded_stacks = super::collapse_stacks(&profiling_data, Duration::from_nanos(1));
 
         let mut expected_stacks = HashMap::<String, u64>::new();
-        expected_stacks.insert("rustc;EventB;EventA".into(), 200);
-        expected_stacks.insert("rustc;EventB".into(), 200);
-        expected_stacks.insert("rustc;EventA".into(), 200);
+        expected_stacks.insert("rustc;e2;e3".into(), 1);
+        expected_stacks.insert("rustc;e2".into(), 2);
+        expected_stacks.insert("rustc;e1".into(), 3);
+        expected_stacks.insert("rustc".into(), 1);
 
         assert_eq!(expected_stacks, recorded_stacks);
     }
-
-    // #[test]
-    // fn multi_threaded_test() {
-    //     let events = [
-    //         Event {
-    //             event_kind: "Query".into(),
-    //             label: "EventA".into(),
-    //             additional_data: &[],
-    //             timestamp: SystemTime::UNIX_EPOCH + Duration::from_secs(1),
-    //             timestamp_kind: TimestampKind::Start,
-    //             thread_id: 1,
-    //         },
-    //         Event {
-    //             event_kind: "Query".into(),
-    //             label: "EventB".into(),
-    //             additional_data: &[],
-    //             timestamp: SystemTime::UNIX_EPOCH + Duration::from_secs(3),
-    //             timestamp_kind: TimestampKind::Start,
-    //             thread_id: 2,
-    //         },
-    //         Event {
-    //             event_kind: "Query".into(),
-    //             label: "EventA".into(),
-    //             additional_data: &[],
-    //             timestamp: SystemTime::UNIX_EPOCH + Duration::from_secs(2),
-    //             timestamp_kind: TimestampKind::End,
-    //             thread_id: 1,
-    //         },
-    //         Event {
-    //             event_kind: "Query".into(),
-    //             label: "EventA".into(),
-    //             additional_data: &[],
-    //             timestamp: SystemTime::UNIX_EPOCH + Duration::from_secs(4),
-    //             timestamp_kind: TimestampKind::Start,
-    //             thread_id: 2,
-    //         },
-    //         Event {
-    //             event_kind: "Query".into(),
-    //             label: "EventA".into(),
-    //             additional_data: &[],
-    //             timestamp: SystemTime::UNIX_EPOCH + Duration::from_secs(5),
-    //             timestamp_kind: TimestampKind::End,
-    //             thread_id: 2,
-    //         },
-    //         Event {
-    //             event_kind: "Query".into(),
-    //             label: "EventB".into(),
-    //             additional_data: &[],
-    //             timestamp: SystemTime::UNIX_EPOCH + Duration::from_secs(6),
-    //             timestamp_kind: TimestampKind::End,
-    //             thread_id: 2,
-    //         },
-    //     ];
-
-    //     let recorded_stacks = super::collapse_stacks(events.iter().cloned(), 1000);
-
-    //     let mut expected_stacks = HashMap::<String, usize>::new();
-    //     expected_stacks.insert("rustc;EventB;EventA".into(), 1);
-    //     expected_stacks.insert("rustc;EventB".into(), 2);
-    //     expected_stacks.insert("rustc;EventA".into(), 1);
-
-    //     assert_eq!(expected_stacks, recorded_stacks);
-    // }
 }
